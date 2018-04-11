7814313bf164a435d04d19c8a6cc353810d06e77
PYC <tamuratarou3@gmail.com>
2018-04-12 02:10:03 +0900

    Update Vita-DLC-NAME.py

--- Vita-DLC-NAME.py
+++ Vita-DLC-NAME.py
@@ -6,15 +6,33 @@ from bs4 import BeautifulSoup
 import re
 
 # アクセスするURL
-url = "https://store.playstation.com/ja-jp/grid/JP0507-PCSG00133_00-0000000000000000/1?relationship=add-ons"
-res = requests.get(url)
-soup = BeautifulSoup(res.text, 'lxml')
+base = "https://store.playstation.com/ja-jp/grid/JP0507-PCSG00418_00-0000000000000000/1?relationship=add-ons"
 
-elems = soup.find_all(href=re.compile("/ja-jp/product/"))
 
-DLC = []
-for e in elems:
-    temp = e.get('href')[15:]
-    if temp not in DLC:
-        print(temp)
-        DLC.append(temp)
+def get_dlc(url):
+    res = requests.get(url, allow_redirects=False)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, 'lxml')
+
+        elems = soup.find_all(href=re.compile("/ja-jp/product/"))
+
+        dlc = []
+        for e in elems:
+            temp = e.get('href')[15:]
+            if temp not in dlc:
+                print(temp)
+                dlc.append(temp)
+    else:
+        return -1
+
+
+def main():
+    for i in range(1, 100):
+        url = base[:-22] + str(i) + "?relationship=add-ons"
+        # print(url)
+        if get_dlc(url) == -1:
+            break
+
+
+if __name__ == '__main__':
+    main()
